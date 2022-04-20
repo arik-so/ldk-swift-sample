c45d50567038956c258109d64098a91c94dbd1d1
Arik Sosman <git@arik.io>
2022-04-20 15:04:49 -0700

    Initial Commit

---
+++ ldk-swift-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ldk-swift-sample
+//
+//  Created by Arik Sosman on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ldk-swift-sample/ldk_swift_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ldk_swift_sampleApp.swift
+//  ldk-swift-sample
+//
+//  Created by Arik Sosman on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ldk_swift_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
